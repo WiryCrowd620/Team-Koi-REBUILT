04bc28b66394abdd63766b4e15905d99439f2bce
Adam <adamkors29@gmail.com>
2026-01-23 16:17:59 +0200

    commit

--- src/main/java/frc/robot/subsystems/feederSubsystem.java
+++ src/main/java/frc/robot/subsystems/feederSubsystem.java
@@ -1,49 +1,30 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.*;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class feederSubsystem extends SubsystemBase {
     private final SparkMax m_SparkMax;
-    private final double power;
 
     /** Creates a new ExampleSubsystem. */
-    public feederSubsystem(double power) {
-        this.power = power;
-        m_SparkMax = new SparkMax(0, MotorType.kBrushless);
+    public feederSubsystem() {
+        m_SparkMax = new SparkMax(Constants.feederConstants.kMotorID, MotorType.kBrushless);
     }
 
-    /**
-     * Example command factory method.
-     *
-     * @return a command
-     */
-    public Command exampleMethodCommand() {
-        // Inline construction of command goes here.
-        // Subsystem::runOnce implicitly requires `this` subsystem.
-        return runOnce(() -> {
-            
-        });
-    }
-
-    public void setWheelVoltage() {
+    public void setVoltage(double power) {
         m_SparkMax.setVoltage(power);
     }
 
-    /**
-     * An example method querying a boolean state of the subsystem
-     * (for example, a digital sensor).
-     *
-     * @return value of some boolean subsystem state
-     */
-    public boolean exampleCondition() {
-        // Query some boolean state, such as a digital sensor.
-        return false;
+    public Command feederMethodCommand(double power) {
+        return runOnce(() -> {
+            setVoltage(power);
+        });
     }
-
+    
     @Override
     public void periodic() {
         // Called once per scheduler run
