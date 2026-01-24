16e3701b4a95f47782f2965e368a3a8bbdeca336
Roi Romem <130078520+RoiRomem@users.noreply.github.com>
2026-01-24 22:36:00 +0200

    Added FeederSubsystem to AllianceZoneShootingCommand

--- src/main/java/frc/robot/commands/AllianceZoneShootingCommand.java
+++ src/main/java/frc/robot/commands/AllianceZoneShootingCommand.java
@@ -1,17 +1,20 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.FeederSubsystem;
 import frc.robot.subsystems.HoodSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
-public class AllianceZoneShooting extends Command {
+public class AllianceZoneShootingCommand extends Command {
     private final ShooterSubsystem shooterSubsystem;
     private final HoodSubsystem hoodSubsystem;
+    private final FeederSubsystem feederSubsystem;
 
-    public AllianceZoneShooting(ShooterSubsystem shooterSubsystem, HoodSubsystem hoodSubsystem) {
+    public AllianceZoneShootingCommand(ShooterSubsystem shooterSubsystem, HoodSubsystem hoodSubsystem, FeederSubsystem feederSubsystem) {
         this.shooterSubsystem = shooterSubsystem;
         this.hoodSubsystem = hoodSubsystem;
-        addRequirements(shooterSubsystem, hoodSubsystem);
+        this.feederSubsystem = feederSubsystem;
+        addRequirements(shooterSubsystem, hoodSubsystem, feederSubsystem);
     }
 
     @Override
