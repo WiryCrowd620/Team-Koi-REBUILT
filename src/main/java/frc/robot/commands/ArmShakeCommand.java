5576292415aa537f8228b7b50ee5f1a3c8105e63
Roi Romem <130078520+RoiRomem@users.noreply.github.com>
2026-01-24 22:25:00 +0200

    Incredible comment

--- src/main/java/frc/robot/commands/ArmShakeCommand.java
+++ src/main/java/frc/robot/commands/ArmShakeCommand.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeArmSubsystem;
 
+/* You're allowed to refer to this command as the thug shake, please do */
 public class ArmShakeCommand extends Command {
     private final IntakeArmSubsystem intakeArmSubsystem;
 
